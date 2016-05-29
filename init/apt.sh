cd52cdda20a196fcdfee503b6ea6835821cee1cb
Jürgen Wagner <juer.wagner@gmail.com>
2016-05-29 14:55:38 +0200

    added apt install script

---
+++ init/apt.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+function setdiff() {
+  local debug skip a b
+  if [[ "$1" == 1 ]]; then debug=1; shift; fi
+  if [[ "$1" ]]; then
+    local setdiffA setdiffB setdiffC
+    setdiffA=($1); setdiffB=($2)
+  fi
+  setdiffC=()
+  for a in "${setdiffA[@]}"; do
+    skip=
+    for b in "${setdiffB[@]}"; do
+      [[ "$a" == "$b" ]] && skip=1 && break
+    done
+    [[ "$skip" ]] || setdiffC=("${setdiffC[@]}" "$a")
+  done
+  [[ "$debug" ]] && for a in setdiffA setdiffB setdiffC; do
+    echo "$a ($(eval echo "\${#$a[*]}")) $(eval echo "\${$a[*]}")" 1>&2
+  done
+  [[ "$1" ]] && echo "${setdiffC[@]}"
+}
+
+
+packages=(
+	curl
+	git
+	vim
+	vim-gui-common
+	build-essentials
+	telnet
+	tree
+	htop
+)
+
+packages=($(setdiff "${packages[*]}" "$(dpkg --get-selections | grep -v deinstall | awk '{print $1}')"))
+
+if (( ${#packages[@]} > 0 )); then
+  e_header "Installing APT packages: ${packages[*]}"
+  for package in "${packages[@]}"; do
+    sudo apt-get -qq install "$package"
+  done
+fi
+
+
