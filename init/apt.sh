e2e3f687945c4a516032375fb91d3b2b9ffd6e00
Manuel Klemenz <m.klemenz@gmx.at>
2016-06-18 00:35:42 +0200

    fix: do not replace inline - get the correct output instead

--- init/apt.sh
+++ init/apt.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-packages=`cat package_sets/* | sed -i -e 's/$/ \\/'`
+packages=`cat package_sets/* | sed -e 's/$/ \\/'`
 installrecommended=`dialog --yesno 'install apt recommendation?' 6 50 || echo -n '--no-install-recommends'`
 withsudo=`which sudo >> /dev/null`
 
