34b8207e33280df056c2963af522683c24e3ade2
Manuel Klemenz <m.klemenz@gmx.at>
2016-06-18 00:37:49 +0200

    fix: use correct sudo command, if available

--- init/apt.sh
+++ init/apt.sh
@@ -2,7 +2,7 @@
 
 packages=`cat package_sets/* | sed -e 's/$/ \\/'`
 installrecommended=`dialog --yesno 'install apt recommendation?' 6 50 || echo -n '--no-install-recommends'`
-withsudo=`which sudo >> /dev/null`
+withsudo=`which sudo || echo -n ''`
 
 e_header "Installing APT packages: ${packages[*]}"
 
